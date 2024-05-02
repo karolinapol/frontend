65e50d72047c6bf58e3cd2d5271f825582355fad
Karolina Polak <karolinapol2001@gmail.com>
2024-05-02 21:37:26 +0200

    3 - fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,24 @@
 <script setup>
+const searchQuery = ref("star")
+const resultQuery = ref( null )
 
+const searchData = async() => {
+    await useFetch('https://images-api.nasa.gov/search?q=' + searchQuery.value).then((response) => {
+        resultQuery.value = response.data
+}) 
+}
+    
+onBeforeMount(() => {
+    searchData()
+});
 </script>
 <template>
- <h3><span>index</span></h3>
+    <div>
+        <input type="text" v-model="searchQuery">
+        <button @click="searchData">Wyszukaj</button>
+    </div>
+
+ 
 </template>
 <style scoped lang="scss">
 h3 {
